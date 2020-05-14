623ba95e9bc7d6ee75163fae60989c0743c6de45
prycedom <prycedom@gmail.com>
2020-05-13 20:36:07 -0400

    Added variables to js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-// Declare at the beginning
-var i = missedTries[0];
+// Variables
 
+var keyboard = document.getElementById (qwerty);
+var newphrase = document.getElementById (phrase);
+var reset = document.getElementsByClassName ('.btn_reset');
+var missed = [0]
+var i = missed;
+i = missed[0];
 // Use later
 for (i = 0; i < 5; i++) {
 
