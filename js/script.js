69d67b1ebae4ed2660ae9c7497afe491ba734ce3
prycedom <prycedom@gmail.com>
2020-05-20 18:36:41 -0400

    Hide overlay

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ const checkWin = () => {
 }
 
 startButton.addEventListener('click', () => {
-
+  document.getElementById("overlay").style.display = "none";
 });
 
 qwerty.addEventListener('click', () => {
