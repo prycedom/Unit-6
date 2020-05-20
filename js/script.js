2dd1c9b80f1597a2e7eeaebab95aa035756a62b3
prycedom <prycedom@gmail.com>
2020-05-20 18:29:34 -0400

    adding const

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 // Variables
 
-var keyboard = document.getElementById (qwerty);
-var newphrase = document.getElementById (phrase);
-var reset = document.getElementsByClassName ('.btn_reset');
-var missed = [0]
-var i = missed;
+const keyboard = document.getElementById (qwerty);
+const newphrase = document.getElementById (phrase);
+const reset = document.getElementsByClassName ('.btn_reset');
+let phrases = ['I love coding', 'I am learning to code', 'I am learning javascript', 'javascript is fun', 'Learning to code is fun']
+const missed = [0]
+const i = missed;
 i = missed[0];
 // Use later
 for (i = 0; i < 5; i++) {
 
 }
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = button => {
+
+}
+
+const checkWin = () => {
+
+}
+
+startButton.addEventListener('click', () => {
+
+});
+
+qwerty.addEventListener('click', () => {
