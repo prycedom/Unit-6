1fd0350168a9b83b7408ed4d7bdf4d849c794df9
prycedom <prycedom@gmail.com>
2020-05-24 20:23:44 -0400

    addPhraseToDisplay

--- js/script.js
+++ js/script.js
@@ -26,11 +26,44 @@ const getRandomPhraseAsArray = arr => {
   return arr [randomNumber]
 }
 getRandomPhraseAsArray(phrases);
-// Checks to see if the letter is in this phrase:
 
-const checkLetter = button => {
+function addPhraseToDisplay(arr){
+  const phraseArray = getRandomPhraseAsArray(phrases);
+  var getLetter =  Math.floor(Math.random() * phrases.length);
+  var i;
+for (i = 0; i < phraseArray.length; i++) {
+  text += phraseArray[i];
+}
+  return arr [getLetter]
 
 }
+
+// addPhrasetoDisplay(phraseArray);
+
+// Checks to see if the letter is in this phrase:
+
+// const checkLetter = button => {
+//   checkLetter.addEventListner('click', () => {
+//     var randomNumber =  Math.floor(Math.random() * phrases.length);
+//     var liLetter = ['I love coding',
+//                     'I am learning to code',
+//                     'I am learning javascript',
+//                     'javascript is fun',
+//                     'Learning to code is fun']
+//     var match = 0;
+//     for (i = 0; i < phrases.length; i++) {
+//         text += arr[liLetter];
+//        if () {
+//          return match;
+//        }
+//        else {
+//
+//        }
+//     }
+//     return arr [liLetter]
+//   });
+
+
 //
 // const addPhraseToDisplay = arr => {
 //
