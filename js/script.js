f1a03a709de28cb9e5f2419e44fef23e2268c466
prycedom <prycedom@gmail.com>
2020-05-24 21:29:06 -0400

    Trying to add displayPhrase

--- js/script.js
+++ js/script.js
@@ -28,40 +28,40 @@ const getRandomPhraseAsArray = arr => {
 getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay(arr){
-  const phraseArray = getRandomPhraseAsArray(phrases);
   var getLetter =  Math.floor(Math.random() * phrases.length);
   var i;
 for (i = 0; i < phraseArray.length; i++) {
-  text += phraseArray[i];
+  return phraseArray[i];
 }
   return arr [getLetter]
 
 }
-
-// addPhrasetoDisplay(phraseArray);
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
 
 // Checks to see if the letter is in this phrase:
 
-// const checkLetter = button => {
-//   checkLetter.addEventListner('click', () => {
-//     var randomNumber =  Math.floor(Math.random() * phrases.length);
-//     var liLetter = ['I love coding',
-//                     'I am learning to code',
-//                     'I am learning javascript',
-//                     'javascript is fun',
-//                     'Learning to code is fun']
-//     var match = 0;
-//     for (i = 0; i < phrases.length; i++) {
-//         text += arr[liLetter];
-//        if () {
-//          return match;
-//        }
-//        else {
-//
-//        }
-//     }
-//     return arr [liLetter]
-//   });
+const checkLetter = button => {
+  checkLetter.addEventListner('click', () => {
+    var randomNumber =  Math.floor(Math.random() * phrases.length);
+    var liLetter = ['I love coding',
+                    'I am learning to code',
+                    'I am learning javascript',
+                    'javascript is fun',
+                    'Learning to code is fun']
+    var match = 0;
+    for (i = 0; i < phrases.length; i++) {
+        text += arr[liLetter];
+       if () {
+         return match;
+       }
+       else {
+
+       }
+    }
+    return arr [liLetter]
+  });
+}
 
 
 //
